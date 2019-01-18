5766522d92df8a8eed87c899d4e79963aef57926
Derek Miller <dgmxm7@gmail.com>
2019-01-18 13:05:53 -0700

    Final ensemble model with comments and updates to constants.

--- PYTHON/LogitEnsemble.py
+++ PYTHON/LogitEnsemble.py
@@ -1,23 +1,23 @@
 import numpy as np
 import utils
+import matplotlib.pyplot as plt
 from constants import *
 
-# X is R,T,A,L
-# Y is R,T
+# X has dimension R,T,A,L
+# Y has dimension R,T
 
-data_dict_hmnl,data_dict = utils.get_data_dict(kind='ensemble',pathology_type='basic')
+data_dict_hmnl,data_dict = utils.get_data_dict(kind='ensemble',pathology_type=pathology_type)
 
 stan_data = {
         'A':nalts,
-        'L':nlvls//2,
+        'L':11,
         'loc':0,
         'scale':2
         }
 
-step = np.linspace(0,N,ntask_train+1).astype(np.int64)
-l_step = np.arange(0,nlvls+1,6)
+step = np.linspace(0,N,K+1).astype(np.int64)
 
-model_list = sorted(['mnl', 'mnl_fhorseshoe']*2)
+model_list = sorted(['mnl']*nlvls)
 
 M = len(model_list)
 
@@ -25,19 +25,24 @@ M = len(model_list)
 Yhat_train = np.zeros((N,nalts,M))
 for k in range(K):
     for m in range(M):
-    
+
         k_fold = np.array([True]*N)
         k_fold[step[k]:step[k+1]] = False
 
-        stan_data['N'] = sum(k_fold) # N-Nk
-        stan_data['Ntest'] = sum(~k_fold) # Nk
+        # set the K-fold temporary values of N and Ntest
+        stan_data['N'] = sum(k_fold)
+        stan_data['Ntest'] = sum(~k_fold)
 
         # new training set = subset of full training set
-        stan_data['X'] = data_dict['Xtrain'][k_fold, :, l_step[m%2]:l_step[m%2 + 1]]
+        # LOVO = Leave One Variable Out
+        Xtrain_lovo = np.delete(data_dict['Xtrain'][k_fold, :, :], m, 2)
+        Xtest_lovo = np.delete(data_dict['Xtrain'][~k_fold, :, :], m, 2)
+
+        stan_data['X'] = Xtrain_lovo
         stan_data['Y'] = data_dict['Ytrain'][k_fold]
     
         # new test set = complement of new training set | full training set
-        stan_data['Xtest'] = data_dict['Xtrain'][~k_fold, :, l_step[m%2]:l_step[m%2 + 1]]
+        stan_data['Xtest'] = Xtest_lovo
     
         base_model = utils.get_model(model_name=model_list[m])
         FIT = utils.fit_model_to_data(base_model, stan_data)
@@ -45,8 +50,6 @@ for k in range(K):
         Yc = FIT.extract(pars=['Yc'])['Yc'].sum(axis=0)
         Yhat_k = np.argmax(Yc, axis=1)
         Yhat_train[~k_fold, Yhat_k, m] += 1
-        #Yhat_train[~k_fold, :, m] = Yc
-
 
 # make predictions on full test set using full training set
 model_scores = []
@@ -57,11 +60,14 @@ for m in range(M):
     stan_data['Ntest'] = Ntest # length of test set
     
     # full training set
-    stan_data['X'] = data_dict['Xtrain'][:, :, l_step[m%2]:l_step[m%2 + 1]]
+    Xtrain_lovo = np.delete(data_dict['Xtrain'], m, 2)
+    Xtest_lovo = np.delete(data_dict['Xtest'], m, 2)
+
+    stan_data['X'] = Xtrain_lovo
     stan_data['Y'] = data_dict['Ytrain']
  
     # full test set
-    stan_data['Xtest'] = data_dict['Xtest'][:,:,l_step[m%2]:l_step[m%2 + 1]]
+    stan_data['Xtest'] = Xtest_lovo
 
     base_model = utils.get_model(model_name=model_list[m])
     FIT = utils.fit_model_to_data(base_model, stan_data)
@@ -69,15 +75,14 @@ for m in range(M):
     Yc_test = FIT.extract(pars=['Yc'])['Yc'].sum(axis=0)
     Yhat_k = np.argmax(Yc_test, axis=1)
     Yhat_test[np.array([True]*Ntest), Yhat_k, m] += 1
-    #Yhat_test[:, :, m] += Yc_test
 
     model_scores.append(Ntest - np.count_nonzero(Yhat_k+1 - data_dict['Ytest']))
 
 
 # Fit stacking model to full test data using augmented training set
 stan_data['M'] = M
-stan_data['Yhat_train'] = Yhat_train
-stan_data['Yhat_test'] = Yhat_test
+stan_data['Yhat_train'] = Yhat_train.copy()
+stan_data['Yhat_test'] = Yhat_test.copy()
 stan_data['L'] = nlvls
 
 meta_model = utils.get_model(model_name='meta_mnl2')
@@ -85,6 +90,7 @@ FIT = utils.fit_model_to_data(meta_model, stan_data)
 
 Yc_stacking = FIT.extract(pars=['Yc'])['Yc'].sum(axis=0)
 Yhat_stacking = np.argmax(Yc_stacking, axis=1) + 1
+model_weights = FIT.extract(pars=['B'])['B']
 
 ensemble_hit_count = Ntest - np.count_nonzero(Yhat_stacking - data_dict['Ytest'])
 
@@ -114,12 +120,12 @@ FIT = utils.fit_model_to_data(base_model, stan_data)
 
 Yc = FIT.extract(pars=['Yc'])['Yc'].sum(axis=0).reshape((Ntest, nalts))
 Yhat = np.argmax(Yc, axis=1) + 1
-print(Yhat)
 
 hit_count = Ntest - np.count_nonzero(Yhat - data_dict_hmnl['Ytest'].reshape(Ntest))
-print("\nMODEL SCORE\n\t",hit_count/Ntest)
+print("\nSTANDARD MODEL SCORE\n\t",hit_count/Ntest)
 print("ENSEMBLE SCORE\n\t",ensemble_hit_count/Ntest)
 print("BASE MODEL SCORES\n\t", np.array(model_scores)/Ntest)
+print("MODEL WEIGHTS\n\t", np.around(model_weights.mean(axis=0),decimals=2))
 
 yy = Yhat_test.sum(axis=2)
 
@@ -131,6 +137,5 @@ coverage = np.array(coverage_list)
 print("BASE MODEL COVERAGE")
 for i in range(M):
     print(i+1, len(coverage[coverage==i+1]))
-print(Yhat_test)
 
 ## END ##
--- PYTHON/constants.py
+++ PYTHON/constants.py
@@ -1,20 +1,33 @@
-nresp = 100
-ntask = 15
-nalts = 4
-nlvls = 12
-ncovs = 1
-niter = 300
-nchains = 2
-treedepth = 3
+"""
+Parameters and constant values for running HBMNL vs ensemble conjoint model.
+"""
+
+### SAMPLER PARAMETERS ###
+niter = 300 # number of iterations in the HMC sampler (Stan param)
+nchains = 2 # number of markov chains (mostly useful for diagnostics)
+treedepth = 3 # how deep to explore the posterior space
 random_seed = 1750532
 
-nresp_train = 100
-ntask_train = 10
-nresp_test = 100
-ntask_test = 5
+### DATA SET PARAMETERS ###
+nresp = 100 # number of respondents
+ntask = 15 # number of tasks (combined training and holdout sets)
+nalts = 4 # number of alternatives or options
+nlvls = 12 # number of feature-levels
+ncovs = 1 # number of covariates (for standard hbmnl)
 
-K = ntask_train # K for k-fold CV
+nresp_train = 100 # number of respondents in the training set
+ntask_train = 10 # number of choice-tasks in the training set
+nresp_test = 100 # number of respondents in the test set
+ntask_test = 5 # number of choice-tasks in the test set
 
-N = nresp_train*ntask_train
-Nk = nresp_train
-Ntest = nresp_train*ntask_test
+pathology_type = 'basic' # describes the pathologies to induce in the data set
+# Options:
+#   'none' : don't induce pathologies on the data set
+#   'screening' : induce a screening pathology (feature variable set to -inf)
+#   'ANA' : induce attribute non-attendence (feature variable set to 0
+#   'basic' : induce 'screening' and 'ANA'
+
+K = 2 # K for k-fold CV
+
+N = nresp_train*ntask_train # number of training observations
+Ntest = nresp_train*ntask_test # number of test observations
