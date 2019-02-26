0d4cbe0047706e19e8640d119c3d706dad98e8a8
Derek Miller <dgmxm7@gmail.com>
2019-02-26 16:21:28 -0700

    Updated gitignore now correctly removes real data sets. Updated demo.py to run models on each data set 10 times and stores the results in a csv file.

--- Code/PYTHON/demo.py
+++ Code/PYTHON/demo.py
@@ -17,32 +17,29 @@ hbmnl_score = list()
 ensemble_score = list()
 hbmnl_time = list()
 ensemble_time = list()
+run_list = list()
 
 for dataset in dataset_list:
-
-    path_to_data = "../../Data/{0}".format(dataset)
-
-    if dataset in dataset_list[4:]:
-        hbmnl_result,ensemble_result = conjoint.model_comparison(path_to_data, holdout=2, init_r=1)
-
-    else:
-        hbmnl_result,ensemble_result = conjoint.model_comparison(path_to_data, holdout=5)
-
-    hbmnl_score.append(hbmnl_result['SCORE'])
-    hbmnl_time.append(hbmnl_result['TIME'])
-    ensemble_score.append(ensemble_result['SCORE'])
-    ensemble_time.append(ensemble_result['TIME'])
-
+    for run in range(10):
+    
+        path_to_data = "../../Data/{0}".format(dataset)
+    
+        if dataset in dataset_list[4:]:
+            hbmnl_result,ensemble_result = conjoint.model_comparison(path_to_data, holdout=2, init_r=1)
+    
+        else:
+            hbmnl_result,ensemble_result = conjoint.model_comparison(path_to_data, holdout=5)
+    
+        hbmnl_score.append(hbmnl_result['SCORE'])
+        hbmnl_time.append(hbmnl_result['TIME'])
+        ensemble_score.append(ensemble_result['SCORE'])
+        ensemble_time.append(ensemble_result['TIME'])
+        run_list.append(run)
+    
 RESULTS['dataset'] = dataset_list
 RESULTS['hbmnl_score'] = hbmnl_score
 RESULTS['ensemble_score'] = ensemble_score
 RESULTS['hbmnl_time'] = hbmnl_time
 RESULTS['ensemble_time'] = ensemble_time
-
-print(RESULTS)
-
-save_data = input("save data? [yes/no] ")
-
-if save_data == "yes":
-    RESULTS.to_csv("./results.csv")
-
+RESULTS['run'] = run_list
+RESULTS.to_csv("./results.csv")
