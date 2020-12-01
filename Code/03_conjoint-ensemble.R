3fcc34c5a94f5a8ea7365bf5d0755bdad9f776a8
Marc Dotson <marc_dotson@byu.edu>
2020-11-30 17:21:28 -0700

    Ensemble running with VB with init = 0; running a full model to initialize VB with posterior means

--- Code/03_conjoint-ensemble.R
+++ Code/03_conjoint-ensemble.R
@@ -10,51 +10,85 @@ library(loo)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-# Load data.
-ind_non <- 1 # Indicates no pathologies.
-ind_scr <- 0 # Indicates screening.
-ind_ana <- 0 # Indicates attribute non-attendance.
-ind_sna <- 0 # Indicates screening and attribute non-attendance.
-
-# Load and format data.
-if (ind_non == 1) {
-  # Matrix of observed choices.
-  Y <- read_csv(here::here("Data", "01_PathologyNone", "Y.csv")) %>%
-    select(-resp) %>%
-    as.matrix()
-
-  # Array of experimental designs per choice task.
-  X_raw <- read_csv(here::here("Data", "01_PathologyNone", "X.csv"))
-}
-
-# Restructure X_raw into X.
-X <- array(NA, dim = c(max(X_raw$resp), max(X_raw$task), max(X_raw$alt), ncol(X_raw) - 3))
-for (n in 1:max(X_raw$resp)) {
-  for (t in 1:max(X_raw$task)) {
-    X[n,t,,] <- X_raw %>%
-      filter(resp == n, task == t) %>%
-      select(contains("l_")) %>%
-      as.matrix()
-  }
-}
-
-# Matrix of respondent-level covariates.
-Z <- rep(1, nrow(Y)) %>%
-  as.matrix()
+# # Load data.
+# ind_non <- 1 # Indicates no pathologies.
+# ind_scr <- 0 # Indicates screening.
+# ind_ana <- 0 # Indicates attribute non-attendance.
+# ind_sna <- 0 # Indicates screening and attribute non-attendance.
+# 
+# # Load and format data.
+# if (ind_non == 1) {
+#   # Matrix of observed choices.
+#   Y <- read_csv(here::here("Data", "01_PathologyNone", "Y.csv")) %>%
+#     select(-resp) %>%
+#     as.matrix()
+# 
+#   # Array of experimental designs per choice task.
+#   X_raw <- read_csv(here::here("Data", "01_PathologyNone", "X.csv"))
+# }
+# 
+# # Restructure X_raw into X.
+# X <- array(NA, dim = c(max(X_raw$resp), max(X_raw$task), max(X_raw$alt), ncol(X_raw) - 3))
+# for (n in 1:max(X_raw$resp)) {
+#   for (t in 1:max(X_raw$task)) {
+#     X[n,t,,] <- X_raw %>%
+#       filter(resp == n, task == t) %>%
+#       select(contains("l_")) %>%
+#       as.matrix()
+#   }
+# }
+# 
+# # Matrix of respondent-level covariates.
+# Z <- rep(1, nrow(Y)) %>%
+#   as.matrix()
 
 # Jeff's data (not working) but still using the indicator matrices.
-load(here::here("Data", "R05_Zerorez", "design.RData"))
-# Y <- data$train_Y
-# X <- data$train_X
-# Z <- matrix(rep(1, nrow(Y)), ncol = 1)
-# mat_ana <- data$mat_ana
+load(here::here("Data", "R06_Ground-Beef", "design.RData"))
+Y <- data$train_Y
+X <- data$train_X
+Z <- matrix(rep(1, nrow(Y)), ncol = 1)
+
+# Work with just two members of the ensemble to start.
 ind_ana <- data$mat_ana[1:2, 1:dim(X)[4]]
+data$mat_ana <- ind_ana
+ind_screen <- data$mat_screen[1:2, 1:dim(X)[4]]
+data$mat_screen <- ind_screen
+
+# Initialize Ensemble with Posterior Means --------------------------------
+stan_data <- list(
+  R = dim(X)[1],    # Number of respondents.
+  S = dim(X)[2],    # Number of choice tasks.
+  A = dim(X)[3],    # Number of choice alternatives.
+  I = dim(X)[4],    # Number of observation-level covariates.
+  J = ncol(Z),      # Number of population-level covariates.
+  
+  Gamma_mean = 0,   # Mean of population-level means.
+  Gamma_scale = 5,  # Scale of population-level means.
+  Omega_shape = 2,  # Shape of population-level scale.
+  tau_mean = 0,     # Mean of population-level scale.
+  tau_scale = 5,    # Scale of population-level scale.
+  
+  Y = Y,            # Matrix of observations.
+  X = X,            # Array of observation-level covariates.
+  Z = Z,            # Matrix of population-level covariates.
+)
+
+initial_fit <- stan(
+  here::here("Code", "Source", "hmnl.stan"),
+  data = stan_data,
+  seed = 42
+)
+
+# Set inital values by providing a list equal in length to the number of chains.
+# The elements of this list should themselves be named lists, where each of
+# these named lists has the name of a parameter and is used to specify the
+# initial values for that parameter for the corresponding chain.
 
 # Ensemble Calibration ----------------------------------------------------
 K <- nrow(ind_ana)
 ensemble_fit <- vector(mode = "list", length = K)
 for (k in 1:K) {
-  data <- list(
+  stan_data <- list(
     R = dim(X)[1],    # Number of respondents.
     S = dim(X)[2],    # Number of choice tasks.
     A = dim(X)[3],    # Number of choice alternatives.
@@ -75,22 +109,22 @@ for (k in 1:K) {
     ind_ana = ind_ana # Matrix of ensemble indicators for ANA.
   )
   
-  ensemble_fit[[k]] <- stan(
-    here::here("Code", "Source", "hmnl_ensemble.stan"),
-    data = data,
-    seed = 42
-  )
-  
-  # # Calibrate the model using variational inference.
-  # ensemble_fit[[k]] <- vb(
-  #   stan_model(here::here("Code", "Source", "hmnl_ensemble.stan")),
-  #   data = data,
+  # ensemble_fit[[k]] <- stan(
+  #   here::here("Code", "Source", "hmnl_ensemble.stan"),
+  #   data = stan_data,
   #   seed = 42
   # )
+  
+  ensemble_fit[[k]] <- vb(
+    stan_model(here::here("Code", "Source", "hmnl_ensemble.stan")),
+    data = stan_data,
+    init = 0,
+    seed = 42
+  )
 }
 
-# Check that fixing values is working.
-k <- 2
+# Check that fixing values is working (for full posterior).
+k <- 1
 beta_ids <- ensemble_fit[[k]] %>%
   gather_draws(Beta[r, i]) %>% 
   filter(r <= 5) %>% 
@@ -119,9 +153,10 @@ ggsave(
 
 which(ind_ana[k,]==1)
 
-# Save ensemble output.
+# Save data and ensemble output.
+data$ensemble_fit <- ensemble_fit
 write_rds(
-  ensemble_fit,
-  here::here("Output", "fit_pathology-none.rds")
+  data,
+  here::here("Output", "fit_vb_pathology-none.rds")
 )
 
