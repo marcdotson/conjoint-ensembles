b806def3d593663f2486825afdb90dee52345013
Roger Bailey <roger.allen.bailey@gmail.com>
2021-02-05 14:16:09 -0500

    Updated model comparison code: Draws from upper level, includes covariates

--- Code/06_model-comparison.R
+++ Code/06_model-comparison.R
@@ -6,8 +6,9 @@ library(loo)
 
 # Source functions.
 source(here::here("Code", "Source", "predictive_fit_ensemble.R"))
-# source(here::here("Code", "Source", "hit_rate.R"))
-# source(here::here("Code", "Source", "hit_prob.R"))
+source(here::here("Code", "Source", "predictive_fit_hmnl.R"))
+# source(here::here("Code", "Source", "predictive_fit_ana.R"))
+# source(here::here("Code", "Source", "predictive_fit_cscreen.R"))
 
 # Load Data, Fit, and Weights ---------------------------------------------
 ind_none <- 0       # Indicates no pathologies.
@@ -25,45 +26,95 @@ hmnl_fit <- read_rds(here::here("Output", str_c("hmnl-fit_", file_name, ".rds"))
 ensemble_fit <- read_rds(here::here("Output", str_c("ensemble-fit_vb_", file_name, ".rds")))
 ensemble_weights <- read_rds(here::here("Output", str_c("ensemble-weights_", file_name, ".rds")))
 
+
 # Compute Model Fit -------------------------------------------------------
 # Extract needed draws.
-hmnl_draws <- extract(hmnl_fit, pars = "Beta")
+hmnl_draws <- extract(hmnl_fit, pars = c("Beta", "Gamma", "Omega", "tau"))
 ensemble_draws <- vector(mode = "list", length = length(ensemble_fit))
 for (k in 1:length(ensemble_fit)) {
-  ensemble_draws[[k]] <- extract(ensemble_fit[[k]], pars = c("Beta", "log_lik"))
+  ensemble_draws[[k]] <- extract(ensemble_fit[[k]], pars = c("Beta", "Gamma", "Omega", "tau", "log_lik"))
 }
 
 # Compute HMNL predictive fit.
-# - Should we have a separate function to generate hold-out sample betas?
-# - Do we care about in-sample hit rate and hit prob?
+hmnl_pred_fit <- predictive_fit_hmnl(
+  hmnl_fit = hmnl_draws, 
+  test_X = data$test_X, 
+  test_Y = data$test_Y,
+  Z=NULL
+)
 
 # Create a model comparision data frame.
 model_comparison <- tibble(
   Model = "HMNL",
   LOO = loo(hmnl_fit)$elpd_loo,
-  "Hit Rate" = NA,
-  "Hit Prob" = NA
+  "Hit Rate" = hmnl_pred_fit$hit_rate[2],
+  "Hit Prob" = hmnl_pred_fit$hit_prob[2],
 )
 
 # Compute fit metrics for ensemble
 ensemble_pred_fit <- predictive_fit_ensemble(
   ensemble_weights = ensemble_weights, 
   ensemble_fit = ensemble_draws, 
-  test_x = data$test_X, 
-  test_y = data$test_Y
+  test_X = data$test_X, 
+  test_Y = data$test_Y,
+  Z=NULL
 )
 
-# # Append results to the model comparison data frame.
-# model_comparison <- model_comparison %>% 
-#   bind_rows(
-#     tibble(
-#       Model = "Ensemble",
-#       LOO = loo_fit_ens$elpd_loo,
-#       "Hit Rate" = hit_rate_vec %*% ensemble_weights,
-#       "Hit Prob" = hit_prob_vec %*% ensemble_weights
-#     )
-#   )
+# Append results to the model comparison data frame.
+model_comparison <- model_comparison %>% 
+   bind_rows(
+     tibble(
+       Model = "Ensemble",
+       LOO = ensemble_pred_fit$loo_fit$elpd_loo,
+       "Hit Rate" = ensemble_pred_fit$hit_rate[2],
+       "Hit Prob" = ensemble_pred_fit$hit_prob[2]
+    )
+   )
 
-# # Save the model comparison data frame.
-# write_rds(model_comparison, here::here("Figures", "model_fit.rds"))
+#Still Need to add competing models here with predictive fit.
+
+# Compute fit metrics for ANA model
+#ana_pred_fit <- predictive_fit_ana(
+#  ana_fit = ana_draws, 
+#  test_X = data$test_X, 
+#  test_Y = data$test_Y,
+#  Z=NULL
+#)
+
+
+# Append results to the model comparison data frame.
+model_comparison <- model_comparison %>% 
+  bind_rows(
+    tibble(
+      Model = "ANA",
+      LOO = NA,
+      "Hit Rate" = NA,
+      "Hit Prob" = NA
+    )
+  )
+
+
+
+#ConjScreen
+# Compute fit metrics for Conj Screening model
+#cscreen_pred_fit <- predictive_fit_cscreen(
+#  cscreen_fit = cscreen_draws, 
+#  test_X = data$test_X, 
+#  test_Y = data$test_Y,
+#  Z=NULL
+#)
+
+# Append results to the model comparison data frame.
+model_comparison <- model_comparison %>% 
+  bind_rows(
+    tibble(
+      Model = "Conj Screen",
+      LOO = NA,
+      "Hit Rate" = NA,
+      "Hit Prob" = NA
+    )
+  )
+
+# Save model comparison data frame.
+write_rds(model_comparison, here::here("Figures", "model_fit.rds"))
 
--- Code/Source/predictive_fit_ensemble.R
+++ Code/Source/predictive_fit_ensemble.R
@@ -1,19 +1,22 @@
-predictive_fit_ensemble = function(ensemble_weights, ensemble_fit, test_x, test_y){
+predictive_fit_ensemble = function(ensemble_weights, ensemble_fit, test_X, test_Y, Z){
   # Compute the hit rate, hit prob, and loo metrics for the ensemble model.
   #   ensemble_weights - estimated weights for each of the models
-  #   ensemble_fit - ensemble output with log_lik and betadraws for each model
+  #   ensemble_fit - ensemble output with log_lik, betadraws, gammas, and Omegas for each model
   #   test_Y - choices (hold-out sample)
   #   test_X - design matrices (hold-out sample)
-  
+  #   Z - matrix of covariates
+
   nens <- length(ensemble_fit)
   ndraw <- length(ensemble_fit[[1]]$Beta[,1,1])         # Number of draws
   nresp <- length(test_Y[,1])           # Number of respondents
   nscns <- length(test_X[1, ,1,1])      # Number of choice tasks
   nalts <- length(test_X[1,1, ,1])      # Number of alternatives 
   nlvls <- length(test_X[1,1,1, ])      # Number of att levels
+  if(is.null(Z)){Z <- matrix(double(nresp)+1, nc = 1)}
+  ncov <- ncol(Z)  # Number of covariates
   
   #weight log_lik for each model to get log_lik for ensemble
-  LLarray_ens = array(0,dims)
+  LLarray_ens = array(0, dim(ensemble_fit[[1]]$log_lik))
   for(k in 1:nens){
     #extract log_lik array from each stanfit object
     LLarray_ens <- LLarray_ens + ensemble_weights[k]*ensemble_fit[[k]]$log_lik
@@ -39,45 +42,87 @@ predictive_fit_ensemble = function(ensemble_weights, ensemble_fit, test_x, test_
   test_Y_stacked <- matrix(t(test_Y),nc=1)
   
   #loop over ensemble models to calculate individual hit rates
-  hit_rate_vec=double(ndraw)
-  hit_prob_vec=double(ndraw)
-  #loop over draws
-  for(draw in 1:ndraw){
-    #pull the posterior mean betas from each model into a matrix for this draw
-    betabar_mat <- matrix(double(nlvls*nens), nc=nlvls)
-    for(n in 1:nens){
-      betadraw <- ensemble_fit[[n]]$Beta
-      #average betadraws over in-sample respondents for hold-out sample (no upper level)
-      betabar_mat[n,] <- colMeans(betadraw[draw,,])
+  probs_ens_postbetas = matrix(0, nr = nalts , nc = nresp*nscns*ndraw)
+  probs_ens_Zgamma = matrix(0, nr = nalts , nc = nresp*nscns*ndraw)
+  probs_ens_rand_het = matrix(0, nr = nalts , nc = nresp*nscns*ndraw)
+  
+  #loop over models
+  for(model in 1:nens){
+    #get betas for 2 different hit rate calculations:
+      #1) using mean of posterior betas as predicted part-worths
+      Umat_postbetas = matrix(0, nr = nresp*nscns*nalts, nc=ndraw)
+      #2) using mean of the distribution of heterogeneity as predicted part worths
+      Umat_Zgamma = matrix(0, nr = nresp*nscns*nalts, nc=ndraw)
+    
+    #loop over respondents
+    for(resp in 1:nresp){
+      #pull betas
+      betadraw <- ensemble_fit[[model]]$Beta
+      #transpose dimensions of betadraw array
+      betadraw <- aperm(betadraw, c(2,1,3))
+      #convert to matrix for faster mean over respondents
+      betadraw <- matrix(betadraw, nr= dim(betadraw)[1])
+      #take mean over respondents to get mean posterior betas for this model
+      post_betabar_mat<- matrix(colMeans(betadraw), nr=ndraw)
+      #identify which betas are set to 0 in this model
+      index <- double(nlvls)
+      index[colSums(post_betabar_mat)!=0] <- 1
+      
+      #use upper level to get mean of dist of heterogeneity
+      gammadraw=ensemble_fit[[model]]$Gamma
+      #transpose dimensions of gammadraw array
+      gammadraw <- aperm(gammadraw, c(2,1,3))
+      #multiply by Z to get mean of dist of het
+      Zgamma_mat <- matrix(Z[resp,]%*%gammadraw, nr=ndraw)
+      #set Zgamma_mat column = 0 if ensemble ignores the level
+      Zgamma_mat[,index==0] <- 0
+      
+      #get utility for each alternative using the three different types
+      Umat_postbetas[((resp-1)*nalts*nscns+1):((resp)*nalts*nscns),] <- 
+                           exp(test_X_stacked[((resp-1)*nalts*nscns+1):((resp)*nalts*nscns),]%*%
+                                            t(post_betabar_mat))
+      Umat_Zgamma[((resp-1)*nalts*nscns+1):((resp)*nalts*nscns),] <- 
+        exp(test_X_stacked[((resp-1)*nalts*nscns+1):((resp)*nalts*nscns),]%*%
+        t(Zgamma_mat))
     }
-    
-    #find probabilities for each
-    Umat <- exp(test_X_stacked%*%t(betabar_mat))
-    Umat <- matrix(Umat, nr = nalts) 
-    sums <- t(matrix(rep(colSums(Umat),nalts), nc=nalts))
-    probs <- Umat/sums
-    
-    #stack probs to weight by ensemble weights
-    probs <- matrix(probs, nc=nens)
-    
-    #get probs for ensemble and reorganize by scn
-    probs_ens <- probs%*%ensemble_weights
-    probs_ens <- matrix(probs_ens, nr=nalts)
-    
-    #find location of highest prob
-    locs <- apply(probs_ens,2,which.max)
-    
-    #identify hitrate and store
-    hits <- double(length(locs))
-    hits[ locs == test_Y_stacked ] <- 1
-    hit_rate_vec[draw]=mean(hits)
-    
-    #identify hitprobs and store
-    prob_select <- colSums(probs_ens*diag(nalts)[,test_Y_stacked])
-    hit_prob_vec[draw] <- mean(prob_select)
+      
+    #find probabilities for each task, resp and draw for Postbetas
+    Umat_postbetas <- matrix(Umat_postbetas, nr = nalts) 
+    sums <- t(matrix(rep(colSums(Umat_postbetas),nalts), nc=nalts))
+    #combine with other model probs weight by ensemble weights 
+    probs_ens_postbetas <- probs_ens_postbetas + 
+                              (Umat_postbetas/sums) *ensemble_weights[model]
+      
+    #find probabilities for each task, resp and draw for Zgamma
+    Umat_Zgamma <- matrix(Umat_Zgamma, nr = nalts) 
+    sums <- t(matrix(rep(colSums(Umat_Zgamma),nalts), nc=nalts))
+    #combine with other model probs weight by ensemble weights 
+    probs_ens_Zgamma <- probs_ens_Zgamma + 
+      (Umat_Zgamma/sums) *ensemble_weights[model]
   }
   
-  hit_rate_ens <- mean(hit_rate_vec)
-  hit_prob_ens <- mean(hit_prob_vec)
-  return(list(hit_prob=hit_prob_ens, hit_rate=hit_rate_ens, loo_fit=loo_fit_ens))
+  #find location of highest prob postbetas
+  locs_postbetas <- apply(probs_ens_postbetas,2,which.max)
+  
+  #find location of highest prob Zgamma
+  locs_Zgamma <- apply(probs_ens_Zgamma,2,which.max)
+      
+  #calculate hits postbetas
+  hits_postbetas <- double(nresp*nscns*ndraw)
+  hits_postbetas[locs_postbetas==rep(test_Y_stacked,ndraw)] <- 1
+  
+  #calculate hit probs postbetas
+  hit_probs_postbetas <- colSums(probs_ens_postbetas*diag(nalts)[,rep(test_Y_stacked,ndraw)])
+  
+  #calculate hits Zgamma
+  hits_Zgamma <- double(nresp*nscns*ndraw)
+  hits_Zgamma[locs_Zgamma==rep(test_Y_stacked,ndraw)] <- 1
+  
+  #calculate hit probs Zgamma
+  hit_probs_Zgamma <- colSums(probs_ens_Zgamma*diag(nalts)[,rep(test_Y_stacked,ndraw)])
+  
+  hitrates=c(mean(hits_postbetas),mean(hits_Zgamma))
+  hitprobs=c(mean(hit_probs_postbetas), mean(hit_probs_Zgamma))
+
+  return(list(hit_prob=hitprobs, hit_rate=hitrates, loo_fit=loo_fit_ens))
 }
