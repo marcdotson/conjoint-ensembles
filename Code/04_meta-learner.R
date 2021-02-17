a6fa3b822bd65c9edda12fffac7bfe95ae3ba81b
Marc Dotson <marc_dotson@byu.edu>
2021-02-16 20:18:09 -0700

    Append ensemble_weights to new ensemble_fit object

--- Code/04_meta-learner.R
+++ Code/04_meta-learner.R
@@ -3,53 +3,32 @@
 library(tidyverse)
 library(rstan)
 library(loo)
+library(parallel)
 
-# Load Data and Ensemble Fit ----------------------------------------------
-# ind_none <- 0       # Indicates no pathologies.
-# ind_ana <- 1        # Indicates attribute non-attendance.
-# ind_screen <- 0     # Indicates screening.
-# ind_ana_screen <- 0 # Indicates attribute non-attendance and screening.
-# hetero <- 1         # Indicates if pathologies differ by individual 
-# nmember <-  100     # Indicate the number of ensemble members.
-# 
-# if (ind_none == 1) file_name <- "none"
-# if (ind_ana == 1) file_name <- "ana"
-# if (ind_screen == 1) file_name <- "screen"
-# if (ind_ana_screen == 1) file_name <- "ana-screen"
-# if (hetero == 1) file_name <- paste(file_name,"-hetero", sep="")
-# if (hetero == 0) file_name <- paste(file_name,"-homo", sep="")
+# Set the simulation seed.
+set.seed(42)
 
-data <- read_rds(here::here("Data", str_c("sim_", file_name, "_", nmember, ".rds")))
-# ensemble_fit <- read_rds(here::here("Output", str_c("ensemble-fit_vb_", file_name, "_", nmember, ".rds")))
-# ensemble_draws <- read_rds(here::here("Output", str_c("ensemble-draws_vb_", file_name, "_", nmember, ".rds")))
+# Load data and ensemble fit.
+data <- read_rds(here::here("Data", str_c("sim_", file_id, ".rds")))
+ensemble_fit <- read_rds(here::here("Output", str_c("ensemble-fit_vb_", file_id, "_", nmember, ".rds")))
 
 # Run the Meta-Learner ----------------------------------------------------
-# # Extract needed draws.
-# ensemble_draws <- vector(mode = "list", length = length(ensemble_fit))
-# for (k in 1:length(ensemble_fit)) {
-#   # ensemble_draws[[k]] <- extract(ensemble_fit[[k]], pars = c("Beta", "log_lik"))
-#   ensemble_draws[[k]] <- extract(ensemble_fit[[k]], pars = c("Gamma", "log_lik"))
-# }
-# # cores <- parallel::detectCores()
-# # # ensemble_draws_test <- parallel::mclapply(ensemble_fit_test, extract, mc.cores = cores)
-# # ensemble_draws <- parallel::mclapply(ensemble_fit, extract, mc.cores = cores - round(cores / 3))
-
 # Create array of likelihoods with effective sample sizes.
-LooPSIS_list <- vector(mode = "list", length = length(ensemble_draws))
-cores <- parallel::detectCores()
-for (k in 1:length(ensemble_draws)) {
-  # Extract log_lik array from each stanfit object.
-  loglik=ensemble_draws[[k]]$log_lik
-  ndraw=dim(loglik)[1]
-  LLmat <- matrix(loglik, nr=ndraw)
+LooPSIS_list <- vector(mode = "list", length = length(ensemble_fit$ensemble_draws))
+for (k in 1:length(ensemble_fit$ensemble_draws)) {
+  # Extract log_lik array from each ensemble draws.
+  loglik <- ensemble_fit$ensemble_draws[[k]]$log_lik
+  ndraw <- dim(loglik)[1]
+  LLmat <- matrix(loglik, nr = ndraw)
+  
   # Get relative effective sample size for each array.
-  r_eff <- relative_eff(x = exp(LLmat), chain_id = double(ndraw)+1, cores = cores)
+  r_eff <- relative_eff(x = exp(LLmat), chain_id = double(ndraw) + 1, cores = detectCores())
+  
   # Apply PSIS via loo to array and save.
-  LooPSIS_list[[k]] = loo.matrix(LLmat, r_eff = r_eff, cores = cores, save_psis = FALSE)
+  LooPSIS_list[[k]] <- loo.matrix(LLmat, r_eff = r_eff, cores = detectCores(), save_psis = FALSE)
 }
 
-# Calculate weights.
-set.seed(22)
+# Apply the meta-learner and calculate ensemble weights.
 ensemble_weights <- loo_model_weights(
   x = LooPSIS_list, 
   method = "stacking", 
@@ -58,8 +37,7 @@ ensemble_weights <- loo_model_weights(
   cores = cores
 )
 
-# Save weights.
-# write_rds(ensemble_weights, here::here("Output", str_c("ensemble-weights_", file_name, ".rds")))
-write_rds(ensemble_weights, here::here("Output", str_c("ensemble-weights_", file_name, "_", nmember, ".rds")))
-# write_rds(ensemble_weights, here::here("Output", str_c("ensemble-weights_", file_name, "_", nmember, "TEST.rds")))
+# Append weights to model fit.
+ensemble_fit <- ensemble_fit$ensemble_weights
+write_rds(ensemble_fit, here::here("Output", str_c("ensemble-fit_", file_id, "_", nmember, ".rds")))
 
