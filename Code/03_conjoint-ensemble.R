c2830ab236f963297f465144d7a7881223a02a42
Marc Dotson <marc_dotson@byu.edu>
2021-05-06 16:26:18 -0600

    Referencing parallel

--- Code/03_conjoint-ensemble.R
+++ Code/03_conjoint-ensemble.R
@@ -212,7 +212,7 @@ fit_extract_average <- function(stan_data) {
   return(ensemble_draws)
 }
 
-ensemble_draws <- parallel::mclapply(stan_data_list, fit_extract_average, mc.cores = detectCores())
+ensemble_draws <- parallel::mclapply(stan_data_list, fit_extract_average, mc.cores = parallel::detectCores())
 
 # Save ensemble fit.
 ensemble_fit <- list(mat_ana = mat_ana, mat_screen = mat_screen, ensemble_draws = ensemble_draws)
