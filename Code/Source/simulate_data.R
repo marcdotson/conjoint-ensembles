339b08a29f4efd2f7cfc95beb5621702b14baec1
Marc Dotson <marc_dotson@byu.edu>
2021-05-20 17:00:16 -0600

    More workflow code review, including commenting simulate_data()

--- Code/Source/simulate_data.R
+++ Code/Source/simulate_data.R
@@ -14,7 +14,8 @@ simulate_data <- function(
   # Function to simulate choice data with or without pathologies.
   require(AlgDesign)
   
-  # Create a full factorial design design and sample from it.
+  # Create a full factorial design and sample from it where the design only has 
+  # discrete attributes and each attribute has the same number of levels.
   data <- gen.factorial(rep(nlevel, times = natt), ntask, center = FALSE)
   indvec <- c(1:nrow(data))
   X.des <- matrix(double(ntask * nalt * nversion * (ncol(data) + 3)), ncol = (ncol(data) + 3))
@@ -40,35 +41,35 @@ simulate_data <- function(
   out = lm(y ~ ., data.tmp, x = TRUE)
   desmat = out$x[,-1]
   
-  # Generate betas and impose pathologies if flagged.
+  # Generate betas and prepare to impose pathologies if flagged.
   nbeta <- natt * nlevel - natt
-  # ana.vec <- rep(1, times = nbeta)
-  # screen.vec <- rep(0, times = nbeta)
   ana.mat <- matrix(double(nbeta * nhh), ncol = nbeta) + 1
   screen.mat <- matrix(double(nbeta * nhh), ncol = nbeta)
   
   for (i in 1:nhh) {
+    # If ANA is flagged, simulate ANA where each respondent pays attention to at least one attribute and 
+    # has non-attendance for at least one attribute.
     if (ana == TRUE) {
-      # ana.draw <- sample(1:nbeta, 3, replace = FALSE)
-      # ana.mat[i,ana.draw] <- 0
       ana.draw <- sample(1:natt, size = round(runif(n = 1, min = 1, max = natt - 1)), replace = FALSE)
       for (j in 1:natt) {
         if (j %in% ana.draw) ana.mat[i, ((j * nlevel - j) - 1):(j * nlevel - j)] <- 0
       }
     }
+    # If screening is flagged, simulate screening where each respondent screens based on one attribute level.
     if (screen == TRUE) {
       screen.draw <- sample(1:nbeta, 1, replace = FALSE)
       screen.mat[i, screen.draw] <- -100
     }
   }
   
-  # Induce randomization in simulated data for respondent data 
-  # forcing random choice if resp == TRUE.
+  # If respondent quality is flagged, randomly determine up to 25 low-quality respondents.
+  force a random choice for up to 25 respondents.
   resp.id <- 0
   if (resp == TRUE) {
     resp.id <- sample(1:nhh, sample(0:25, 1), replace = FALSE)
   }
   
+  # If heterogeneity isn't flagged, have the first iteration of ANA and screening apply for all members.
   if (hetero == FALSE) {
     ana.vec <- ana.mat[1,]
     screen.vec <- screen.mat[1,]
@@ -78,6 +79,7 @@ simulate_data <- function(
     }
   }
   
+  # Generate average betas.
   bbar <- runif(nbeta, -1, 2)
   
   # Generate Y|X.
@@ -91,14 +93,15 @@ simulate_data <- function(
     screen.vec <- screen.mat[i,]
     for (j in 1:ntask) {
       xtmp <- as.matrix(tmp[which(tmp[,2] == j), 4:(ncol(desmat))])
+      # Generate betas as a deviation from the average.
       betah <- bbar + rnorm(length(bbar), 0, 1)
+      # Multiply betas by an ANA indicator and add screening (defaults to 1 and 0, respectively).
       betah <- betah * ana.vec + screen.vec
       U <- as.vector(xtmp %*% betah)
       prob.y <- exp(U) / sum(exp(U))
+      # If respondent quality is flagged, force a random choice for the up to 25 respondents.
       if (i %in% resp.id) prob.y <- prob.y * 0 + 1 / nalt
       Y[j] <- sample(1:nalt, 1, prob = prob.y)
-      #U <- as.vector(xtmp %*% betah) - as.vector(log(-log(runif(nalt)))) + resp.err
-      #Y[j] = which(U == max(U))
       X[,,j] = xtmp
     }
     regdata[[i]] = list(X = X, Y = Y)
